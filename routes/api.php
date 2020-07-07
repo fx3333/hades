96f4b394c1c869931b5202b910f97e15d5c640dd
ningbohanbaoge/fengxiaoning <fengxiaoning@hanbaoyufang.com>
2020-07-07 12:00:11 +0800

    api路由修改

--- routes/api.php
+++ routes/api.php
@@ -26,23 +26,25 @@ Route::prefix('v1')
         Route::get('version', function() {
             return 'this is version v1';
         })->name('version');
+
+        Route::prefix('user/')->group(function() {
+            //用户注册
+            Route::post('register', 'UserController@register')->name('user.register');
+            //用户登录
+            Route::post('login', 'UserController@login')->name('user.login');
+            Route::post('authorizations/current', 'UserController@updateToken')->name('user.login');
+        
+            Route::middleware('api.refresh')->group(function() {
+                //当前用户信息
+                Route::get('info', 'UserController@info')->name('user.info');
+                Route::post('changepw', 'UserController@changePw')->name('user.changepw');
+                //用户退出
+                Route::delete('authorizations/current', 'UserController@logout')->name('users.logout');
+            });
+        });
 		
 	});
 
 
-Route::prefix('user/')->group(function() {
-    //用户注册
-    Route::post('register', 'UserController@register')->name('user.register');
-    //用户登录
-    Route::post('login', 'UserController@login')->name('user.login');
-    Route::post('authorizations/current', 'UserController@updateToken')->name('user.login');
 
-    Route::middleware('api.refresh')->group(function() {
-        //当前用户信息
-        Route::get('info', 'UserController@info')->name('user.info');
-        Route::post('changepw', 'UserController@changePw')->name('user.changepw');
-        //用户退出
-        Route::delete('authorizations/current', 'UserController@logout')->name('users.logout');
-    });
-});
 
